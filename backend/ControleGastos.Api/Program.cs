7702a6cdd017648fbc49821df5e0b4cd6293e48a
gmadalosso <gbmadalosso@gmail.com>
2026-01-14 08:59:03 -0300

    atualização CORS

--- backend/ControleGastos.Api/Program.cs
+++ backend/ControleGastos.Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5174")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
